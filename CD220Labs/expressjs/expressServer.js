c55c52427f1fb8f82b793c5acd78d429f2a68525
fabiomfaria <fabiomfaria@gmail.com>
2022-02-22 22:57:19 -0300

    upload do app

---
+++ CD220Labs/expressjs/expressServer.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = new express();
+
+let loginDetails = [];
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+app.get("/fetch/:num",(req,res)=>{
+    let num = parseInt(req.params.num);
+    if(num <1 || num >12) {
+        res.send("Not a valid month number")
+    } else {
+        res.send(months[num-1])
+    }
+
+})
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the express server")
+})
+
+app.get("/loginDetails",(req,res)=>{
+    res.send(JSON.stringify(loginDetails));
+})
+
+app.post("/login/:name",(req,res)=>{
+    loginDetails.push({"name":req.params.name,"login_time":new Date()});
+    res.send(req.params.name + ", You are logged in!")
+})
+
+app.get("/:name",(req,res)=>{
+    res.send("Hello "+req.params.name)
+})
+
+app.listen(3333, () => {
+    console.log(`Listening at http://localhost:3333`)
+})
+
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = new express();
+
+app.get("/", (req,res) => {
+    return res.send("Hello World!");
+});
+
+let server = app.listen(0, () => {
+    console.log("listening at http://localhost:"+server.address())
+})
